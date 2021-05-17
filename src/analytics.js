b2bd3cd36dff738ff0d362a2c8579cd18a2db121
Sylvain Filteau <cidsphere@gmail.com>
2021-05-17 17:57:04 -0400

    use gtag

--- src/analytics.js
+++ src/analytics.js
@@ -1,10 +1,5 @@
-// analytics.js
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-export const init = () => {
-    // Initialize the command queue in case analytics.js hasn't loaded yet.
-    window.ga = window.ga || ((...args) => (ga.q = ga.q || []).push(args));
-
-    ga('create', 'G-0XEKCE0M1P', 'auto');
-    ga('set', 'transport', 'beacon');
-    ga('send', 'pageview');
-};
+gtag('config', 'G-0XEKCE0M1P');
